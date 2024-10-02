3cdbf6e4b5d03c21da556ae0bf42d9a13ccdf8c1
LXiangEthan <l.xiang.ethan@gmail.com>
2024-10-02 17:18:03 +0800

    注释说明

---
+++ config.js
@@ -0,0 +1,22 @@
+const siteConfig = {
+    sites: [
+        { name: "GitHub", url: "https://github.com/LXiangEthan", image: "https://ethanliang.top/upload/github.svg" },
+        { name: "Alist", url: "https://alist.ethanliang.top", image: "https://ethanliang.top/upload/枫叶.svg" },
+        { name: "生活日记", url: "https://hexo.ethanliang.top", image: "https://hexo.ethanliang.top/img/favior.svg" },
+        { name: "个人博客", url: "https://ethanliang.top", image: "https://ethanliang.top/upload/Trilium.svg" },
+        { name: "notion分享", url: "https://notion.ethanliang.top", image: "https://notion.ethanliang.top/icon.svg" }
+    ],
+    title: "Ethan的个人门户",
+    introduction: {
+        name: "Ethan",
+        avatar: "https://ethanliang.top/upload/20171014085116_A38Qr-wgme.png",
+        description: "网站开发者 | 编程爱好者 | 技术工作者",
+        details: "欢迎来到我的个人门户网站，我是一位个人开发者，如果你对我感兴趣，可以通过下面路径了解我，我们一起钻研技术，一起学习，一起探讨问题，如果你对其他的感兴趣我们也可以聊一聊"
+    },
+    weatherAPI: {
+        url: "https://uapis.cn/api/weather",
+        params: {
+            name: "济南市"
+        }
+    }
+};
